050b63c87d98f5d9aee7c62a3e31dbbaa19e7b94
narendra539804 <64107227+narendra539804@users.noreply.github.com>
2021-12-23 13:23:48 +0530

    saving in the db

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const cafeList = document.querySelector("#cafe-list");
+const form = document.querySelector("#add-cafe-form");
 
 // create element & render cafe
 function renderCafe(doc) {
@@ -24,3 +25,16 @@ db.collection("cafes")
       renderCafe(doc);
     });
   });
+
+// saving data
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  db.collection("cafes").add({
+    name: form.name.value,
+    city: form.city.value,
+  });
+  console.log(form.name.value);
+  console.log(form.city.value);
+  form.name.value = "";
+  form.city.value = "";
+});
